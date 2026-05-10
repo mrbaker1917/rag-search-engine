db682e11a2fa55d051cd5634433e4caa8b519cec
mrbaker1917 <mrbaker1917@gmail.com>
2026-05-10 12:06:55 -0700

    adds semantic embedding of query and verrify embeddings

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,16 +1,43 @@
-
+import numpy as np
+import os
 from sentence_transformers import SentenceTransformer
+
+from lib.search_utils import load_movies
 
 class SemanticSearch:
     def __init__(self, model_name='all-MiniLM-L6-v2') -> None:
         self.model = SentenceTransformer(model_name)
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
     
     def generate_embedding(self, text):
         if len(text.strip()) == 0:
             raise ValueError("Please enter words")
         embeddings = self.model.encode([text])
         return embeddings[0]
-
+    
+    def build_embeddings(self, documents):
+        self.documents = documents
+        doc_strings = []
+        for doc in self.documents:
+            self.document_map[doc["id"]] = doc
+            doc_strings.append(f"{doc['title']}: {doc['description']}")
+        self.embeddings = self.model.encode(doc_strings, show_progress_bar=True)
+        np.save("cache/movie_embeddings.npy", self.embeddings)
+        return self.embeddings
+    
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        for doc in self.documents:
+            self.document_map[doc['id']] =  doc
+        if os.path.exists("cache/movie_embeddings.npy"):
+            self.embeddings = np.load("cache/movie_embeddings.npy")
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+        else:
+            self.embeddings = self.build_embeddings(documents)
+            return self.embeddings
 
 def verify_model():
     search_instance = SemanticSearch()
@@ -24,3 +51,17 @@ def embed_text(text):
     print(f"Text: {text}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Dimensions: {embedding.shape[0]}")
+
+def verify_embeddings():
+    search_instance = SemanticSearch()
+    movies = load_movies()
+    embeddings = search_instance.load_or_create_embeddings(movies)
+    print(f"Number of docs:   {len(movies)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
+
+def embed_query_text(query):
+    search_instance = SemanticSearch()
+    embedding = search_instance.generate_embedding(query)
+    print(f"Query: {query}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Shape: {embedding.shape}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model, embed_text
+from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
     subparsers.add_parser("verify", help="Verify that the embedding model is loaded.")
+    subparsers.add_parser("verify_embeddings", help="verifies that embeddings have loaded.")
     embed_text_parser = subparsers.add_parser("embed_text", help="provides embedding for text")
     embed_text_parser.add_argument("text", type=str)
+    embed_query_parser = subparsers.add_parser("embed_query", help="Embeds query text.")
+    embed_query_parser.add_argument("query", type=str)
     args = parser.parse_args()
 
     match args.command:
+        case "embed_query":
+            embed_query_text(args.query)
+        case "verify_embeddings":
+            verify_embeddings()
         case "embed_text":
             embed_text(args.text)
         case "verify":
