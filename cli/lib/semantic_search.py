ff0c1d6852d665f15be9480b23bbc572a9bbda85
mrbaker1917 <mrbaker1917@gmail.com>
2026-05-11 17:22:12 -0700

    adds semantic search functions and chunking

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -35,9 +35,31 @@ class SemanticSearch:
             self.embeddings = np.load("cache/movie_embeddings.npy")
             if len(self.embeddings) == len(documents):
                 return self.embeddings
-        else:
-            self.embeddings = self.build_embeddings(documents)
-            return self.embeddings
+        self.embeddings = self.build_embeddings(documents)
+        return self.embeddings
+        
+    def search(self, query, limit):
+        if self.embeddings is None:
+            raise ValueError("No embeddings loaded. Call `load_or_create_embeddings` first.")
+        embedded_query = self.generate_embedding(query)
+        sim_scores = []
+        for i, doc in enumerate(self.embeddings):
+            sim_scores.append((cosine_similarity(embedded_query, doc), self.documents[i]))
+        sorted_sim_scores = sorted(sim_scores, key=lambda t: t[0], reverse=True)
+        list_top_scores = []
+        for tup in sorted_sim_scores[:limit]:
+            d = {"score": tup[0], "title": tup[1]["title"], "description": tup[1]["description"]}
+            list_top_scores.append(d)
+        return list_top_scores
+
+def search_term(term, limit):
+    search_instance = SemanticSearch()
+    movies = load_movies()
+    embeddings = search_instance.load_or_create_embeddings(movies)
+    results = search_instance.search(term, limit)
+    for i, r in enumerate(results, 1):
+        print(f"{i}. {r['title']} (score: {r['score']:.4f})\n {r['description'][:100]+"..."}")
+
 
 def verify_model():
     search_instance = SemanticSearch()
@@ -65,3 +87,13 @@ def embed_query_text(query):
     print(f"Query: {query}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Shape: {embedding.shape}")
+
+def cosine_similarity(vec1, vec2):
+    dot_product = np.dot(vec1, vec2)
+    norm1 = np.linalg.norm(vec1)
+    norm2 = np.linalg.norm(vec2)
+
+    if norm1 ==  0 or norm2 == 0:
+        return 0.0
+    
+    return dot_product / (norm1 * norm2)
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text
+from lib.semantic_search import verify_model, embed_text, verify_embeddings, embed_query_text, search_term
+from lib.search_utils import DEFAULT_SEARCH_LIMIT
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -12,9 +13,29 @@ def main():
     embed_text_parser.add_argument("text", type=str)
     embed_query_parser = subparsers.add_parser("embed_query", help="Embeds query text.")
     embed_query_parser.add_argument("query", type=str)
+    search_parser = subparsers.add_parser("search", help="enter a term to search movie database")
+    search_parser.add_argument("search_term", type=str)
+    search_parser.add_argument("--limit", type=int, default=DEFAULT_SEARCH_LIMIT, help="Search limit")
+    chunk_parser = subparsers.add_parser("chunk", help="divides string into chunks")
+    chunk_parser.add_argument("text", type=str, help="text to be chunked")
+    chunk_parser.add_argument("--chunk-size", type=int, default=200)
+    
     args = parser.parse_args()
 
     match args.command:
+        case "chunk":
+            arr = args.text.split(" ")
+            n = args.chunk_size
+            arr_len = len(arr)
+            print(f"Chunking {len(args.text)} characters")
+            chunks = []
+            for i in range(0, arr_len, n):
+                chunks.append(arr[i:i+n])
+            for i, chunk in enumerate(chunks, 1):
+                print(f"{i}. {" ".join(chunk)}")
+
+        case "search":
+            search_term(args.search_term, args.limit)
         case "embed_query":
             embed_query_text(args.query)
         case "verify_embeddings":
