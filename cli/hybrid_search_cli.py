ff15af2cb343663f581abeffe668d73c5aa98759
mrbaker1917 <mrbaker1917@gmail.com>
2026-05-22 09:18:37 -0700

    adds rrf-search

--- cli/hybrid_search_cli.py
+++ cli/hybrid_search_cli.py
@@ -1,6 +1,6 @@
 import argparse
 
-from lib.hybrid_search import normalize_scores, weighted_search
+from lib.hybrid_search import normalize_scores, weighted_search, rrf_search
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Hybrid Search CLI")
@@ -12,9 +12,16 @@ def main() -> None:
     weighted_search_parser.add_argument("--alpha", type=float, default=0.5, help="Determines weighting of searches.")
     weighted_search_parser.add_argument("--limit", type=int, default=5, help="Determines how many results you want.")
     
+    rrf_search_parser = subparsers.add_parser("rrf-search", help="Allows Reciprocal Rank Fusion searches")
+    rrf_search_parser.add_argument("query", type=str, help="Query term to search in database comparing search rankings")
+    rrf_search_parser.add_argument("-k", type=int, default=60, help="k value to compare rankings")
+    rrf_search_parser.add_argument("--limit", type=int, default=5, help="Limits number of search results.")
+
     args = parser.parse_args()
 
     match args.command:
+        case "rrf-search":
+            rrf_search(args.query, args.k, args.limit)
         case "weighted-search":
             weighted_search(args.query, args.alpha, args.limit)
         case "normalize":
--- cli/lib/hybrid_search.py
+++ cli/lib/hybrid_search.py
@@ -63,7 +63,41 @@ class HybridSearch:
         ]
 
     def rrf_search(self, query, k, limit=10):
-        raise NotImplementedError("RRF hybrid search is not implemented yet.")
+        bm25_results = self._bm25_search(query, limit=limit * 500)
+
+        chunked_sem_results = self.semantic_search.search_chunks(
+            query, limit=limit * 500
+        )
+        rrf_dic = {}
+        for i, res in enumerate(bm25_results, 1):
+            doc = self.idx.docmap[res[0]]
+            rrf_dic[res[0]] = {
+                "title": doc["title"],
+                "description": doc["description"],
+                "BM25": i,
+                "Semantic": limit * 500 + 1,
+                "rrf": 0.0,
+            }
+
+        for j, r in enumerate(chunked_sem_results, 1):
+            if r["id"] in rrf_dic:
+                rrf_dic[r["id"]]["Semantic"] = j
+            else:
+                doc = self.idx.docmap[r["id"]]
+                rrf_dic[r["id"]] = {
+                    "title": doc["title"],
+                    "description": doc["description"],
+                    "BM25": limit * 500 + 1,
+                    "Semantic": j,
+                    "rrf": 0.0,
+                }
+        for i, d in rrf_dic.items():
+
+            rrf_score = 1 / (k + d["BM25"]) + 1 / (k + d["Semantic"])
+            rrf_dic[i]["rrf"] = rrf_score
+        return sorted(rrf_dic.items(), key=lambda item: item[1]["rrf"], reverse=True)[
+            :limit
+        ]
 
 
 def normalize_scores(scores: list[float]) -> list[float]:
@@ -90,4 +124,14 @@ def weighted_search(query, alpha=0.5, limit=5):
     for i, r in enumerate(results, 1):
         print(
             f"{i}. {r[1]['title']}\n Hybrid Score: {r[1]['Hybrid']:.3f}\n BM25: {r[1]['BM25']:.3f}, Semantic: {r[1]['Semantic']:.3f}\n {r[1]['description'][:100]}"
+        )
+
+
+def rrf_search(query, k, limit):
+    movies = load_movies()
+    hybrid_search = HybridSearch(movies)
+    results = hybrid_search.rrf_search(query, k, limit)
+    for i, r in enumerate(results, 1):
+        print(
+            f"{i}. {r[1]['title']}\n RRF Score: {r[1]['rrf']:.3f}\n BM25: {r[1]['BM25']:.3f}, Semantic: {r[1]['Semantic']:.3f}\n {r[1]['description'][:100]}"
         )
